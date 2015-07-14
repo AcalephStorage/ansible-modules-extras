7d99f58a2e0893a80ca1e39ab130306cf0336317
Alistair A. Israel <aisrael@gmail.com>
2015-07-14 15:54:39 +0800

    Support more parameters, use direct JSON API calls (avoid shelling out to Ceph CLI)

--- cloud/ceph/ceph_osd_pool.py
+++ cloud/ceph/ceph_osd_pool.py
@@ -23,7 +23,7 @@ DOCUMENTATION = '''
 module: ceph_osd_pool
 short_description: Ceph pools module for Ansible.
 description:
- - Use this module to manage Ceph pools
+    - Use this module to manage Ceph pools
 version_added: "1.9"
 options:
     name:
@@ -56,11 +56,36 @@ options:
               multiple copies of the objects or C(erasure) to get a kind of generalized RAID5 capability.
               The C(replicated) pools require more raw storage but implement all Ceph operations.
               The C(erasure) pools require less raw storage but only implement a subset of the available operations.
+    erasure_code_profile:
+        required: false
+        description:
+            - For C(erasure) pools only. Use the specified erasure code profile, which must be an existing profile
+              as defined by C(osd erasure-code-profile set).
     ruleset:
         required: false
         description:
             - The name of the crush ruleset for this pool. If specified ruleset doesn’t exist, the creation of
               C(replicated) pool will fail with C(-ENOENT).
+    client_name:
+        required: false
+        default: "client.admin"
+        description:
+            - The client name for authentication
+    cluster:
+        required: false
+        default: "ceph"
+        description:
+            - The cluster name
+    conf:
+        required: false
+        default: ""
+        description:
+            - The Ceph configuration file to use
+    connect_timeout:
+        required: false
+        default: 5
+        description:
+            - The timeout value for connecting to the cluster
 
 requirements:
  - ceph
@@ -70,93 +95,137 @@ author: Alistair Israel
 EXAMPLES = '''
 # Create a pool
 - ceph_osd_pool: state=present name=data pgnum=128 pgpnum=128
+# Create an erasure coded pool_type and specify erasure_code_profile as well as crush ruleset
+- ceph_osd_pool: name=ecpool pgnum=32 pgpnum=32 type=erasure erasure_code_profile=default ruleset=erasure-code
 '''
 
 import datetime
 import json
 
 import rados
-
+from ceph_argparse import json_command
 
 def main():
     module = AnsibleModule(
         argument_spec = dict(
-            state = dict(choices=['present', 'absent'], default='present'),
-            name = dict(required=True),
-            pgnum = dict(type='int'),
-            pgpnum = dict(type='int'),
-            type = dict(choices=['replicated', 'erasure'], default='replicated'),
-            ruleset = dict()
+            state =                 dict(choices=['present', 'absent'], default='present'),
+            name =                  dict(required=True),
+            pgnum =                 dict(type='int'),
+            pgpnum =                dict(type='int'),
+            type =                  dict(choices=['replicated', 'erasure'], default='replicated'),
+            erasure_code_profile =  dict(),
+            ruleset =               dict(),
+            client_name =           dict(default="client.admin"),
+            cluster =               dict(default="ceph"),
+            conf =                  dict(default=""),
+            connect_timeout =       dict(type="int", default=5),
         ),
         supports_check_mode = False
     )
 
     name = module.params['name']
 
-    result = {}
-    result['name'] = name
-
-    def sh(cmd):
-        startd = datetime.datetime.now()
-        rc, out, err = module.run_command(cmd)
-        endd = datetime.datetime.now()
-        delta = endd - startd
-        result['rc'] = rc
-        result['cmd'] = cmd
-        result['stdout'] = out.rstrip("\r\n")
-        result['stderr'] = err.rstrip("\r\n")
-        result['start'] = str(startd)
-        result['end'] = str(endd)
-        result['delta'] = str(delta)
+    startd = datetime.datetime.now()
+    result = dict(
+        name = name,
+        start = str(startd)
+    )
 
     changed = False
 
-    with rados.Rados(conffile='/etc/ceph/ceph.conf') as cluster:
-        pools = cluster.list_pools()
+    client_name = module.params["client_name"]
+    cluster_name = module.params["cluster"]
+    conffile = module.params["conf"]
+
+    try:
+        cluster_handle = rados.Rados(name=client_name, clustername=cluster_name,
+                                     conffile=conffile)
+    except rados.Error as e:
+        module.fail_json(msg="Error initializing cluster client: {0}".format(repr(e)))
+
+    try:
+        timeout = module.params['connect_timeout']
+        cluster_handle.connect(timeout=timeout)
+    except Exception as e:
+        module.fail_json(msg="Error connecting to cluster: {0}".format(e.__class__.__name__))
+
+    try:
+        pools = cluster_handle.list_pools()
 
         state = module.params['state']
         if state == 'present':
             if name not in pools:
-                pgnum = module.params['pgnum']
-                if not pgnum:
+                pg_num = module.params['pgnum']
+                if not pg_num:
                     module.fail_json(msg='pgnum is required when state=present')
-                pgpnum = module.params['pgpnum']
-                type = module.params['type']
+                pgp_num = module.params['pgpnum']
+                pool_type = module.params['type']
                 ruleset = module.params['ruleset']
+                erasure_code_profile = module.params['erasure_code_profile']
+                # unused expected_num_objects
 
-                base_cmd = 'ceph osd pool create {0} {1}'
+                args = {
+                    'prefix': 'osd pool create',
+                    'pool': name,
+                    'pool_type': pool_type,
+                    'pg_num': pg_num
+                }
 
-                if ruleset:
-                    if not pgpnum:
-                        module.fail_json(msg='pgpnum is required when ruleset specified')
-
-                if type == 'erasure':
-                    if not pgpnum:
+                if ruleset and not pgp_num:
+                    module.fail_json(msg='pgpnum is required when ruleset specified')
+                if pool_type == 'erasure':
+                    if not pgp_num:
                         module.fail_json(msg='pgpnum is required when type=erasure')
-                    cmd = (base_cmd + ' {2} erasure').format(name, pgnum, pgpnum)
+                    args['pgp_num'] = pgp_num
+                    if erasure_code_profile:
+                        args['erasure_code_profile'] = erasure_code_profile
                     if ruleset:
-                        cmd = cmd + ' ' + ruleset
+                        args['ruleset'] = ruleset
+                # pool_type = 'replicated'
                 else:
                     if ruleset:
-                        cmd = (base_cmd + ' {2} replicated {3}').format(name, pgnum, pgpnum, ruleset)
-                    elif pgpnum:
-                        cmd = (base_cmd + ' {2}').format(name, pgnum, pgpnum)
-                    else:
-                        cmd = base_cmd.format(name, pgnum)
+                        args['erasure_code_profile'] = ruleset
+                    if pgp_num:
+                        args['pgp_num'] = pgp_num
 
-                sh(cmd)
+                target = ("mon", "")
 
-                expected_stderr = "pool '{0}' created".format(name)
-                changed = expected_stderr == result['stderr']
+                rc, outbuf, outs = json_command(cluster_handle, target=target, argdict=args)
+
+                if rc:
+                    module.fail_json(msg="Error: {0} {1}".format(rc, errno.errorcode[rc]))
+
+                expected_outs = "pool '{0}' created".format(name)
+                changed = expected_outs == outs
+                result['outbuf'] = outbuf
+                result['outs'] = outs
         else:
             if name in pools:
-                sh('ceph osd pool delete {0} {0} --yes-i-really-really-mean-it'.format(name))
+                target = ("mon", "")
+                args = {
+                    'prefix': 'osd pool delete',
+                    'pool': name,
+                    'pool2': name,
+                    'sure': '--yes-i-really-really-mean-it'
+                }
+                rc, outbuf, outs = json_command(cluster_handle, target=target, argdict=args)
 
-                expected_stderr = "pool '{0}' removed".format(name)
-                changed = expected_stderr == result['stderr']
+                if rc:
+                    module.fail_json(msg="Error: {0} {1}".format(rc, errno.errorcode[rc]))
 
-    result['changed'] = changed
-    module.exit_json(**result)
+                expected_outs = "pool '{0}' removed".format(name)
+                changed = expected_outs == outs
+                result['outbuf'] = outbuf
+                result['outs'] = outs
+
+        endd = datetime.datetime.now()
+        result['end'] = str(endd)
+        result['delta'] = str(endd - startd)
+        result['changed'] = changed
+        module.exit_json(**result)
+
+    finally:
+        cluster_handle.shutdown()
 
 # import module snippets
 from ansible.module_utils.basic import *
